c293fad15a4f31cc738abbe80a45bf6b3c74c6ef
Tharun Pranav Sakthivel <sv.tharunpranav@gmail.com>
2023-12-27 21:50:00 -0800

    feat(editor): define initial layout data

---
+++ constants/shapePresets.js
@@ -0,0 +1,49 @@
+/** Desktop shape presets */
+export const shapePresets = {
+  vertical: { w: 3, h: 6 },
+  horizontal: { w: 6, h: 3 },
+  square: { w: 3, h: 3 },
+  doubleHorizontal: { w: 6, h: 6 },
+  halfHorizontal: { w: 6, h: 1.5 },
+
+  //SECTION TITLE
+  wider: { w: 6, h: 2 },
+  full: { w: 12, h: 2 },
+  long: { w: 12, h: 2 },
+
+  //CAROUSEL
+  fullCarousel: { w: 12, h: 6 },
+  halfCarousel: { w: 6, h: 6 },
+
+  //MARQUEE
+  fullMarquee: { w: 12, h: 3 },
+  halfMarquee: { w: 6, h: 3 },
+  verticalMarquee: { w: 3, h: 6 },
+  //BANNER
+  banner: { w: 12, h: 3 },
+};
+
+/** Mobile shape presets */
+export const mobileShapePresets = {
+  vertical: { w: 1, h: 8 },
+  horizontal: { w: 2, h: 4 },
+  square: { w: 1, h: 4 },
+  doubleHorizontal: { w: 2, h: 8 },
+  halfHorizontal: { w: 2, h: 2 },
+
+  //SECTION TITLE
+  wider: { w: 2, h: 2.25 },
+  full: { w: 2, h: 2.25 },
+  long: { w: 2, h: 2.25 },
+
+  //CAROUSEL
+  halfCarousel: { w: 2, h: 8 },
+  fullCarousel: { w: 2, h: 8 },
+
+  //MARQUEE
+  fullMarquee: { w: 2, h: 4 },
+  halfMarquee: { w: 1, h: 4 },
+  verticalMarquee: { w: 1, h: 8 },
+  //BANNER
+  banner: { w: 2, h: 4 },
+};
